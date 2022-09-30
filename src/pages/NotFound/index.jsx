7f9a42312a272246c757d5b6092233654cd4b15a
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-09-30 15:29:02 -0300

    NotFound: criação e estilização da pagina não encontrada

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const logo = require('../../assets/logo.png');
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found" className="page-not-found">
+        <img src={ logo.default } alt="logo" className="logo" />
+        <div className="text-page">
+          <h1>Ops!</h1>
+          <p>A página que você está procurando não foi encontrada.</p>
+        </div>
+        <nav>
+          <Link to="/search">
+            Retornar à Home
+          </Link>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
