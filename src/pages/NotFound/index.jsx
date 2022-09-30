d5cbbad340880a21465aee00191c49485b0d1418
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-09-30 20:54:38 -0300

    notFound: implementa pagina caso pagina não for encontrada

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css';
+import './notFound.css';
 
 const logo = require('../../assets/logo.png');
 
@@ -8,7 +8,7 @@ class NotFound extends React.Component {
   render() {
     return (
       <div data-testid="page-not-found" className="page-not-found">
-        <img src={ logo.default } alt="logo" className="logo" />
+        <img src={ logo.default } alt="logo" className="logo-page-not-found" />
         <div className="text-page">
           <h1>Ops!</h1>
           <p>A página que você está procurando não foi encontrada.</p>
