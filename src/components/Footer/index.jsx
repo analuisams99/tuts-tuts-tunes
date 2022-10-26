3af07ead802d281231475d264c95eea5f1b19350
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-10-25 21:53:53 -0300

    Footer: Criação e estilização do rodapé da aplicação

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>
+          Copyright © 2022 | Ana Luisa Marques Simões.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
