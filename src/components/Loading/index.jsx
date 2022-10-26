59acda633cadfaa48ac686ec77525e71498f372c
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-10-25 21:55:31 -0300

    Loading: Criação e estilização do componente de carregamento da aplicação

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './loading.css';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <div className="loading">
+        <h1>
+          Carregando...
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Loading;
